f9e57ae485cf3c3c147371f6769c66d26ec7a473
fawwazAlkarmy <alkarmyfawwaz@gmail.com>
2023-03-30 22:53:37 +0300

    Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
   return (
-    <div className="App">
-      <Button colorScheme="teal">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024px
+      }}
+    >
+      <GridItem bg="orange.300" area={"nav"}>
+        nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink.300" area={"aside"}>
+          aside
+        </GridItem>
+      </Show>
+      <GridItem bg="yellow.300" area={"main"}>
+        main
+      </GridItem>
+    </Grid>
   );
 }
 
