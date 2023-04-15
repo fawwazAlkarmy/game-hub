e3cb25a70cf93b107a1f67edaf0e46c7fd6e1612
fawwazAlkarmy <alkarmyfawwaz@gmail.com>
2023-04-15 11:26:11 +0300

    filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,44 @@
-import useGenres from "../hooks/useGenres";
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
-  const { data } = useGenres();
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
+  const { data, loading, error } = useGenres();
+  if (loading) return <Spinner />;
+  if (error) return null;
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li>{genre.name}</li>
+        <ListItem paddingY="5px">
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              alt={genre.name}
+              boxSize="32px"
+              borderRadius={8}
+            />
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              variant="link"
+              fontSize="lg"
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 export default GenreList;
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,36 +1,44 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { AxiosError, CanceledError } from "axios";
+import { AxiosError, AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  dependencies?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    const getAllGames = async () => {
-      try {
-        const response = await apiClient.get<FetchResponse<T>>(endpoint, {
-          signal: controller.signal,
-        });
-        setData(response.data.results);
-        setLoading(false);
-      } catch (err) {
-        if (err instanceof CanceledError) return;
-        setError((err as AxiosError).message);
-        setLoading(false);
-      }
-    };
-    getAllGames();
-    return () => controller.abort();
-  }, []);
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      setLoading(true);
+      const getData = async () => {
+        try {
+          const response = await apiClient.get<FetchResponse<T>>(endpoint, {
+            signal: controller.signal,
+            ...requestConfig,
+          });
+          setData(response.data.results);
+          setLoading(false);
+        } catch (err) {
+          if (err instanceof CanceledError) return;
+          setError((err as AxiosError).message);
+          setLoading(false);
+        }
+      };
+      getData();
+      return () => controller.abort();
+    },
+    dependencies ? [...dependencies] : []
+  );
 
   return { data, error, loading };
 };
